f8926b95281b38e2fee2eda421b5c326ab65b625
ivegapalacios <152659898+ivegapalacios@users.noreply.github.com>
2024-01-20 19:28:25 -0300

    version final juego numero secreto

---
+++ app1.js
@@ -0,0 +1,70 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos == 1) ? 'Intento' : 'Intentos'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //el usuario no acertó
+        if (numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El número es menor');
+        } else {
+            asignarTextoElemento('p', 'El número es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //si ya sorteamos todos los numeros
+    if (listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
+    } else {
+        //hacer un barrido de la lista para saber si el numero generado está incluido en la misma
+        if (listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar la caja
+    limpiarCaja();
+    //indicar mensaje de intervalo de numero
+    //generar el numero aleatorio
+    //inicializar el número de intentos
+    condicionesIniciales();
+    //deshabilitar el boton de nuevo juego
+}
+
+condicionesIniciales();

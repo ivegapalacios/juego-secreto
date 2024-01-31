e284c61c681b52f4174730ddb1a7a8492902ba84
ivegapalacios <152659898+ivegapalacios@users.noreply.github.com>
2024-01-31 15:35:22 -0300

    Update app1.js

--- app1.js
+++ app1.js
@@ -33,8 +33,6 @@ function limpiarCaja() {
 
 function generarNumeroSecreto() {
     let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
-    console.log(numeroGenerado);
-    console.log(listaNumerosSorteados);
     //si ya sorteamos todos los numeros
     if (listaNumerosSorteados.length == numeroMaximo){
         asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
